dbdbf879ceca39bff10ca6db4861842779091257
tharundasari18 <tharundasari18@gmail.com>
2026-04-09 16:34:54 -0500

    Delete athena directory

--- athena/queries.sql
+++
@@ -1,5 +0,0 @@
-CREATE EXTERNAL TABLE ipl_winners (...); ##have to specify which columns you have to choose and create in external table
-
-MSCK REPAIR TABLE ipl_winners;
-
-SELECT * FROM ipl_winners WHERE year='2015';
